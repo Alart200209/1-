fa7d75fac7fb77471c1a3ef68d5c64ca9c039e3b
artem alekseev <alart090902@gmail.com>
2020-09-18 13:46:54 +0300

    изменил на английчский язык

--- 1.c
+++ 1.c
@@ -1,20 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <locale.h>
 int main()
 {
-	setlocale(LC_ALL, "Rus");//Переход на русский язык
 	int i = 1;//Минамальное натуральное число 
 	int max = 0;
 	while (i != 0) //Проверка на неравенство с нулем
 	{
-		printf("Введите число=");
+		printf("Input number=");
 		scanf("%d", &i);//Ввод числа
 		if (i > max)
 		{
 			max = i;
 		}
 	}
-	printf("Максимально число %d", max);
+	printf("Max number %d", max);
 	return 0;
 }
